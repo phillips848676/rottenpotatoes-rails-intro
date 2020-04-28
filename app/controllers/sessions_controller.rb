cf391093f2522727b1b57b9390690a56c0aba9df
EC2 Default User <ec2-user@ip-172-31-31-172.us-east-2.compute.internal>
2020-04-28 18:37:55 +0000

    finishing up omni

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,26 @@
 class SessionsController < ApplicationController
     # skip_before_filter :set_current_user
-    @id = 0
     def index
-
-        # @current_user = Moviegoer.where(:uid => @id)
-
+        
+        if (session[:user_id])
+            @current_user = Moviegoer.where(:uid =>session[:user_id])
+        else
+            @current_user = Moviegoer.where(:uid => 0)
+        end
     end
     def create
         auth=request.env["omniauth.auth"]
         logger.debug(auth)
         user = Moviegoer.new(:provider => auth["provider"], :uid => auth["uid"],:name => auth["info"]["name"])
         user.save
-        @id = user.uid
+        @name = user.name
         session[:user_id] = user.uid
         redirect_to root_path
+        
     end
     def destroy
         session.delete(:user_id)
         user = Moviegoer.where(:uid =>session[:user_id])
-        @id = 0
         user.destroy_all
         flash[:notice] = 'Logged out successfully.'
         redirect_to root_path
