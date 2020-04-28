88580c2127f7847b9d84af9f16fd9b0022e3d37e
EC2 Default User <ec2-user@ip-172-31-31-172.us-east-2.compute.internal>
2020-04-28 17:49:23 +0000

    omni

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   skip_before_filter :set_current_user
   def create
     auth=request.env["omniauth.auth"]
-    user=Moviegoer.where(:provider => auth["provider"], :uid => auth["uid"]) ||
-      Moviegoer.create_with_omniauth(auth)
-    session[:user_id] = user.ids
+    # user=Moviegoer.where(:provider => auth["provider"], :uid => auth["uid"]) ||
+    #   Moviegoer.create_with_omniauth(auth)
+    # session[:user_id] = user.ids
     redirect_to movies_path
   end
   def destroy
-    session.delete(:user_id)
+    # session.delete(:user_id)
     flash[:notice] = 'Logged out successfully.'
     redirect_to movies_path
   end
